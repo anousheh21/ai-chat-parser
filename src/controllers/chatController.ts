db6e6e83cf3a7dc5c3a600107dec39318064a7ce
anousheh21 <anousheh1@gmail.com>
2026-07-19 10:14:49 +0100

    Defined tasks

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -1,13 +1,17 @@
 import type { Request, Response } from "express";
 import { chatLinkToJson } from "../services/chatToJson.js";
-import { checkValidUrl } from "../services/checkValidUrl.js";
+import { checkValidChatGPTShareLink } from "../services/checkValidChatGPTShareLink.js";
 
 export const chatControllerPost = async (req: Request, res: Response) => {
     const chatGPTShareLink = req.body.chatGPTShareLink;
 
-    // checkValidUrl(chatGPTShareLink);
-    const jsonChat = await chatLinkToJson(chatGPTShareLink);
+    // Testing that the checkValidUrl function works
+    const validUrl = checkValidChatGPTShareLink(chatGPTShareLink);
+    res.status(200).json(validUrl);
 
-    // This is what we are sending back, so this needs to be the chat as a JSON array
-    res.status(200).json(jsonChat);
+    // TODO: Uncomment this, as this is the actual app flow. It is commented for now, so I can test the checkValidUrl function.
+    // const jsonChat = await chatLinkToJson(chatGPTShareLink);
+
+    // // This is what we are sending back, so this needs to be the chat as a JSON array
+    // res.status(200).json(jsonChat);
 }
---
+++ src/services/checkValidChatGPTShareLink.ts
@@ -0,0 +1,15 @@
+export const checkValidChatGPTShareLink = (chatGPTShareLink: URL) => {
+    // Todo: check chatGPTShareLink is a valid ChatGPT share link
+
+    // 1. Check the link starts with https://chatgpt.com/share/
+
+
+    // 2. Is there a particular format for the identifier? If there is, check the identifier follows that format
+
+
+    // 3. Check that when the page is visited, it actually contains a chat (this may be the serialization thing)
+
+
+    // Should return true or false, depending on whether it's a valid link or not
+    return chatGPTShareLink;
+}
