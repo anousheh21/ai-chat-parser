999f900848b6bfe239b7ef4db054342a7978607b
anousheh21 <anousheh1@gmail.com>
2026-07-21 19:45:04 +0100

    Wrote README v1

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ app.use(express.json());
 // Routes
 app.use(chatRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 })
